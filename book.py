5e305e588fb5e62acf3fd06f4d9d72a1001c9a2c
tnxp21 <tnxp2001@gmail.com>
2021-12-30 12:11:14 +0700

    init
    add old socket

---
+++ book.py
@@ -0,0 +1,20 @@
+ID_ReadBook = {
+    "111": "\nThis is the book_1\n",
+    "112": "\nThis is the book_2\n",
+    "113": "\nThis is the book_3\n",
+    "114": "\nThis is the book_4\n",
+    "115": "\nThis is the book_5\n",
+    "116": "\nThis is the book_6\n",
+    "117": "\nThis is the book_7\n",
+    "118": "\nThis is the book_8\n",
+}
+
+type_of_books = {
+    "Economics" : ["111", "112", "113", "114"],
+    "Computer Science" : ["115", "116", "117", "118"]
+}
+
+author_of_books = {
+    "Author A" : ["111", "113", "115", "117"],
+    "Author B" : ["112", "114", "116", "118"]
+}
